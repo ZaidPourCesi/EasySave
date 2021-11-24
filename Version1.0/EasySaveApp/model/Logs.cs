891cb2fd1aa6233024392f606e010d4c7bc3dafc
aldsanms <73821252+aldsanms@users.noreply.github.com>
2021-11-24 17:36:14 +0100

    final

---
+++ Version1.0/EasySaveApp/model/Logs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+
+namespace EasySaveV1.model
+{ 
+    // The class log is used to take note of few informations about the backups :
+    // target path, source path, name, date, duration and size
+    class Logs
+    {
+        public string sourceRepository { get; set; } // Declaration of the setter/getter for the target repository path
+        public string targetRepository { get; set; } // Declaration of the setter/getter target repository path
+        public string nameToSave { get; set; } // Declaration of the setter/getter for the name of the backup
+        public string backupDate { get; set; } // Declaration of the setter/getter for the date of the backup
+        public string transactionTime { get; set; } // Declaration of the setter/getter for the time of the backup
+        public long totalSize { get; set; } // Declaration of the setter/getter for the total size of the file
+    }
+}
+
